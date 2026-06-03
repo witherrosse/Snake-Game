8bbfc7deb329e0377aad5d9c1d77dd7edef0af99
Ali Fooladi <alifooladiwither@gmail.com>
2026-06-03 11:15:07 +0330

    first commit

--- Food.py
+++ Food.py
@@ -3,14 +3,18 @@ import random
 
 class Food(Turtle):
 
+    ### Create the food object as a small red circle ###
+
     def __init__(self):
 
         super().__init__()
         self.shape("circle")
         self.color("Red")
         self.penup()
-        self.shapesize(0.5,0.5)
+        self.shapesize(0.5, 0.5)   ### Make the food smaller than default###
         self.speed("fastest")
+
+    ### Move food to a random position on the screen ###
 
     def refresh(self):
 
--- SnakeGame.py
+++ SnakeGame.py
@@ -3,30 +3,41 @@ from Food import Food
 from Snake import Snake
 from ScoreBoard import ScoreBoard
 import time
-from types import new_class
+
+## Setup the game window ###
+
 
 screen = Screen()
 screen.bgcolor("black")
 screen.title("Snake Game")
 screen.setup(width=600, height=600)
-screen.tracer(0)
+screen.tracer(0)   # Turn off animation for manual control
+
+### Create game objects ###
 
 snake = Snake()
 food = Food()
 scoreboard = ScoreBoard()
 
+### Keyboard controls ###
+
 screen.listen()
-screen.onkey(snake.up , "w")
-screen.onkey(snake.down , "s")
-screen.onkey(snake.left , "a")
-screen.onkey(snake.right , "d")
+screen.onkey(snake.up, "w")
+screen.onkey(snake.down, "s")
+screen.onkey(snake.left, "a")
+screen.onkey(snake.right, "d")
 
 game_is_on = True
 food.refresh()
+
+###  Main game loop ###
 while game_is_on:
-    screen.update()
-    time.sleep(0.1)
+    screen.update()      ### Refresh the screen ###
+
+    time.sleep(0.1)      ### Control game speed###
     snake.move()
+
+    ### Check if snake eats food ###
 
     if snake.head.distance(food) < 15:
 
@@ -34,33 +45,16 @@ while game_is_on:
         snake.extend()
         scoreboard.increase_score()
 
+    ### Check if snake hits the wall ###
+
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
 
         scoreboard.reset()
         snake.reset()
 
-
+    ### Check if snake hits its own body ###
     for segment in snake.segments[1:]:
-        if snake.head.distance(segment) < 10:
 
+        if snake.head.distance(segment) < 10:
             scoreboard.reset()
             snake.reset()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
