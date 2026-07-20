44d427d1ce0f2d4dfe68905499e14aa3c02e23ea
Ali Fooladi <alifooladiwither@gmail.com>
2026-07-20 11:10:00 +0330

    Update SnakeGame.py

--- Snake_Game/SnakeGame.py
+++ Snake_Game/SnakeGame.py
@@ -4,22 +4,22 @@ from Snake import Snake
 from ScoreBoard import ScoreBoard
 import time
 
-## Setup the game window ###
+
 
 
 screen = Screen()
 screen.bgcolor("black")
 screen.title("Snake Game")
 screen.setup(width=600, height=600)
-screen.tracer(0)   # Turn off animation for manual control
+screen.tracer(0)   
 
-### Create game objects ###
+
 
 snake = Snake()
 food = Food()
 scoreboard = ScoreBoard()
 
-### Keyboard controls ###
+
 
 screen.listen()
 screen.onkey(snake.up, "w")
@@ -30,14 +30,14 @@ screen.onkey(snake.right, "d")
 game_is_on = True
 food.refresh()
 
-###  Main game loop ###
+
 while game_is_on:
-    screen.update()      ### Refresh the screen ###
+    screen.update()      
 
     time.sleep(0.1)      ### Control game speed ###
     snake.move()
 
-    ### Check if snake eats food ###
+    
 
     if snake.head.distance(food) < 15:
 
@@ -45,14 +45,14 @@ while game_is_on:
         snake.extend()
         scoreboard.increase_score()
 
-    ### Check if snake hits the wall ###
+    
 
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
 
         scoreboard.reset()
         snake.reset()
 
-    ### Check if snake hits its own body ###
+   
     for segment in snake.segments[1:]:
 
         if snake.head.distance(segment) < 10:
