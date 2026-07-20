57017eae62e2a4c55555b331497ef1ab37a28167
Ali Fooladi <alifooladiwither@gmail.com>
2026-07-20 11:08:57 +0330

    Update Snake.py

--- Snake_Game/Snake.py
+++ Snake_Game/Snake.py
@@ -15,16 +15,20 @@ class Snake:
         self.create_snake()
         self.head = self.segments[0]
 
-    ###  Create the snake with 3 segments at start ###
+    
 
     def create_snake(self):
+
+        ''' Create the snake with 3 segment at start ''' 
 
         for position in STARTING_POSITION:
             self.add_segment(position)
 
-    ###  Add one new segment to the snake ###
+    
 
     def add_segment(self, position):
+
+        ''' Add one new segment to the snake '''
 
         new_segment = Turtle("square")
         new_segment.color("white")
@@ -32,9 +36,11 @@ class Snake:
         new_segment.goto(position)
         self.segments.append(new_segment)
 
-    ### Reset the snake for a new game ##
+    
 
     def reset(self):
+
+        ''' Reset the snake for a new game '''
 
         for seg in self.segments:
             seg.goto(1000, 1000)   ### Move old segments far away ###
@@ -42,15 +48,19 @@ class Snake:
         self.create_snake()
         self.head = self.segments[0]
 
-    ### Make the snake longer when it eats food ##3
+    
 
     def extend(self):
 
+        ''' Make the snake longer when it eats food '''
+
         self.add_segment(self.segments[-1].position())
 
-    ### Move the snake forward ###
+    
 
     def move(self):
+
+        ''' Move the snake forward ''' 
 
         for seg_num in range(len(self.segments) - 1, 0, -1):
             new_x = self.segments[seg_num - 1].xcor()
@@ -58,30 +68,39 @@ class Snake:
             self.segments[seg_num].goto(new_x, new_y)
         self.head.forward(MOVE_DISTANCE)
 
-    ### Change direction to up (cannot go back down) ###
+    
 
     def up(self):
+
+        ''' Change direction to up (cannot go back down) '''
 
         if self.head.heading() != DOWN:
             self.head.setheading(UP)
 
-    ### Change direction to down (cannot go back up) ###
+    
 
     def down(self):
+
+        ''' Change direction to down (cannot go back up) '''
 
         if self.head.heading() != UP:
             self.head.setheading(DOWN)
 
-    ### Change direction to left (cannot go back right) ###
+    
 
     def left(self):
 
+        ''' Change direction to left (cannot go back right) '''
+
         if self.head.heading() != RIGHT:
+            
             self.head.setheading(LEFT)
 
-    ### Change direction to right (cannot go back left) ###
+    
 
     def right(self):
+
+        ''' Change direction to right (cannot go back left) '''
 
         if self.head.heading() != LEFT:
             self.head.setheading(RIGHT)
