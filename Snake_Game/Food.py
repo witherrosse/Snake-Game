74ee02e04f14c13b3e58b9bf4c2b89b940f47574
Ali Fooladi <alifooladiwither@gmail.com>
2026-07-20 11:04:25 +0330

    Update Food.py

--- Snake_Game/Food.py
+++ Snake_Game/Food.py
@@ -3,20 +3,24 @@ import random
 
 class Food(Turtle):
 
-    ### Create the food object as a small red circle ###
+    
 
     def __init__(self):
+
+        ''' Create the food object as a small red circle '''
 
         super().__init__()
         self.shape("circle")
         self.color("Red")
         self.penup()
-        self.shapesize(0.5, 0.5)   ### Make the food smaller than default###
+        self.shapesize(0.5, 0.5)   
         self.speed("fastest")
 
-    ### Move food to a random position on the screen ###
+    
 
     def refresh(self):
+
+        ''' Move food to a random position on the screen '''
 
         x_pos = random.randint(-280, 280)
         y_pos = random.randint(-280, 280)
